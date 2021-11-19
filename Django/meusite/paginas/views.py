9cfa01fc3632e1a7450461475e082be05d4df524
Josue <josuecarlosos2@gmail.com>
2021-11-19 16:49:28 -0300

    trabalhando com arquivos de static

--- Django/meusite/paginas/views.py
+++ Django/meusite/paginas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(req):
+    return render(req, 'paginas/index.html')
+
+
+def sobre(req):
+    return render(req, 'paginas/sobre.html')
